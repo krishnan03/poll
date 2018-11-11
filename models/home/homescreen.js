d63dd77c011f245e8a7025855c663768efe6dbd1
karuppig <Aug@2018>
2018-11-11 19:50:34 +0530

    Added Icon

--- models/home/homescreen.js
+++ models/home/homescreen.js
@@ -1,14 +1,32 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Text, View,Image,Button } from 'react-native';
 import { TabNavigator } from 'react-navigation';
 import PollScreen from '../poll/poll';
 import ProfileScreen from '../profile/profile';
 import SearchScreen from '../search/search';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import UserDetailsModal from '../userDetailsModal';
+
+
 class HomeScreen extends React.Component {
+
+  static navigationOptions={
+    tabBarIcon:({tintColor})=>(
+    <Icon name="home" size={20} color={ tintColor } />
+    )
+  }
+  openModal(){
+    this.refs.addDetails.showModal();
+  }
   render() {
     return (
       <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
         <Text>Home!</Text>
+        <Button
+          title="User Modal"
+          onPress={()=> this.openModal()}>
+        </Button>
+        <UserDetailsModal ref={'addDetails'}></UserDetailsModal>
       </View>
     );
   }
@@ -24,7 +42,12 @@ var MainScreenNavigator=TabNavigator({
   Poll:{screen: PollScreen},
   Search:{screen: SearchScreen},
 },{
-  tabBarPosition:'bottom'
+  tabBarPosition:'bottom',
+  tabBarOptions:{
+    tabBarLabel: null,
+    activeTintColor:'white',
+    showIcon: true
+  }
 });
 
 
